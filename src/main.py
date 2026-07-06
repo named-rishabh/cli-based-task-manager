3fd55633b65ad5ae3a33b9ed7598808c081ea97e
named-rishabh <rishabhyadav.dev@gmail.com>
2026-07-06 13:24:28 +0530

    minor change in main.py

--- src/main.py
+++ src/main.py
@@ -9,13 +9,6 @@ def greeting():
     username, name = my_login.user_availability()
     print(f"Welcome, {name}")
 
-    # if len(sys.argv) < 2:
-    #     print("Usage: python columns.py DIRECTORY")
-    # else:
-    #     directory = os.listdir(sys.argv[1])
-    #     columns = Columns(directory, equal=True, expand=True)
-    #     print(columns)
-
 
 if __name__ == "__main__":
     greeting()
